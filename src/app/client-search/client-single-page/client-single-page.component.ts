7de8271210bf21d4f62d6195b633cb66b9984a48
kyara-code <chiara.ko.26@gmail.com>
2022-04-12 15:16:36 +0200

    project

--- src/app/client-search/client-single-page/client-single-page.component.ts
+++ src/app/client-search/client-single-page/client-single-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { WebPage } from './../../model/page.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-client-single-page',
   templateUrl: './client-single-page.component.html',
-  styleUrls: ['./client-single-page.component.css']
+  styleUrls: ['./client-single-page.component.css'],
 })
 export class ClientSinglePageComponent implements OnInit {
+  @Input() page: WebPage;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/service/http-requests.service.ts
+++ src/app/service/http-requests.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { WebPage } from '../model/page.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpRequestsService {
-  searchInput: string = null;
-
   constructor(private http: HttpClient) {}
 
-  searchPage() {
-    return this.http.get('http://localhost:3000/ricerca?q=' + this.searchInput);
+  searchPage(searchInput: string) {
+    console.log('Your search: ' + searchInput);
+    return this.http.get<WebPage[]>(
+      'http://localhost:3000/ricerca?chiavi_q=' + searchInput
+    );
   }
 }
