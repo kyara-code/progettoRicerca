c649f2a4ea15670110ecf0c06530374432bf8073
kyara-code <chiara.ko.26@gmail.com>
2022-04-13 16:47:09 +0200

    project

--- src/app/service/http-requests.service.ts
+++ src/app/service/http-requests.service.ts
@@ -7,12 +7,28 @@ import { WebPage } from '../model/page.model';
   providedIn: 'root',
 })
 export class HttpRequestsService {
+  filterSearchValue = '';
+  items = [
+    { id: 0, name: 'key' },
+    { id: 1, name: 'title' },
+    { id: 2, name: 'description' },
+    { id: 3, name: 'language' },
+    { id: 4, name: 'most recent first' },
+  ];
+
   constructor(private http: HttpClient, private authService: AuthService) {}
 
   searchPage(searchInput: string) {
     console.log('Your search: ' + searchInput);
+    console.log(
+      'http://localhost:3000/ricerca?' +
+        this.filterSearchValue +
+        '_like=' +
+        searchInput
+    );
     return this.http.get<WebPage[]>(
-      'http://localhost:3000/ricerca?chiavi_like=' + searchInput
+      'http://localhost:3000/ricerca?q=' + searchInput
+      //ricerca da cambiare in qualcosa_like= se ti serve qualcosa di specifico
     );
     // fare tre get: uno per la ricerca tra le chiavi (do 3 punti),
     // uno per la ricerca tra i titoli, do due punti,
@@ -22,14 +38,40 @@ export class HttpRequestsService {
 
   postPage(webSite: WebPage) {
     this.http
-      .post('http://localhost:3000/ricerca', webSite,
-      {
-        headers: new HttpHeaders({ Authorization: 'Bearer ' + this.authService.token }),
+      .post('http://localhost:3000/ricerca', webSite, {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + this.authService.token,
+        }),
       })
-      .subscribe((response) => {console.log(response)});
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
-  deletePage() {
-    
-  }
+  deletePage() {}
+
+  // getFilter(id: number) {
+  //   switch (id) {
+  //     case 0:
+  //       this.filterSearchValue = 'chiavi';
+  //       console.log(this.filterSearchValue);
+  //       break;
+  //     case 1:
+  //       this.filterSearchValue = 'titolo';
+  //       console.log(this.filterSearchValue);
+  //       break;
+  //     case 2:
+  //       this.filterSearchValue = 'descrizione';
+  //       console.log(this.filterSearchValue);
+  //       break;
+  //     case 3:
+  //       this.filterSearchValue = 'lingua';
+  //       console.log(this.filterSearchValue);
+  //       break;
+  //     case 4:
+  //       this.filterSearchValue = 'data';
+  //       console.log(this.filterSearchValue);
+  //       break;
+  //   }
+  // }
 }
