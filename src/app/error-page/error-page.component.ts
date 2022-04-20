17c657f21cee796db1403f243b140a32390bceb3
kyara-code <chiara.ko.26@gmail.com>
2022-04-20 11:08:26 +0200

    project

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,14 +1,30 @@
+import { interval, Subscription } from 'rxjs';
 import { AuthService } from './../service/auth.service';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
   styleUrls: ['./error-page.component.css'],
 })
-export class ErrorPageComponent implements OnInit {
+export class ErrorPageComponent implements OnInit, OnDestroy {
+  @ViewChild('snowman') snowman: ElementRef = null;
+  @ViewChild('fire') fire: ElementRef = null;
+
   error = null;
   authTriedBefore = false;
+
+  isJumping = false;
+  isAnimationOn = true;
+  subscribe: Subscription = null;
+  isGameOver: boolean = false;
 
   constructor(private authService: AuthService) {}
 
@@ -16,6 +32,41 @@ export class ErrorPageComponent implements OnInit {
     this.error = this.authService.error;
     if (this.error.status !== 'Unknown Route') {
       this.authTriedBefore = true;
+    }
+
+    // interval(300).subscribe((count) => {
+    //   let bottomFire = this.fire.nativeElement.getBoundingClientRect().bottom;
+    //   let leftSnowman = this.snowman.nativeElement.getBoundingClientRect().left;
+    //   let leftFire = this.fire.nativeElement.getBoundingClientRect().left;
+    //   let bottomSnowman =
+    //     this.fire.nativeElement.getBoundingClientRect().bottom;
+    //   if (leftFire === leftSnowman) {
+    //     console.log('game over');
+    //     this.isGameOver = true;
+    //   }
+    // });
+  }
+
+  onJump() {
+    this.isJumping = true;
+    setTimeout(() => {
+      this.isJumping = false;
+    }, 800);
+
+    console.log(this.fire.nativeElement.getBoundingClientRect().bottom);
+    console.log(this.snowman.nativeElement.getBoundingClientRect().bottom);
+  }
+
+  onStopAnimation() {
+    this.isAnimationOn = !this.isAnimationOn;
+    if (this.subscribe) {
+      this.subscribe.unsubscribe();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscribe) {
+      this.subscribe.unsubscribe();
     }
   }
 }
