56eb94898490467101c39361bba0bc2a233690b1
ChristianKareemCianci <ckcianci@gmail.com>
2022-04-21 11:04:21 +0200

    project

--- src/app/admin-search/display/display.component.ts
+++ src/app/admin-search/display/display.component.ts
@@ -14,7 +14,6 @@ import { PagesManagerService } from 'src/app/service/pages-manager.service';
 export class DisplayComponent implements OnInit, OnDestroy {
   arrayPages: WebPage[] = [];
   searchInput = '';
-  sectionNumberMax: number;
   subscribe: Subscription;
   subscribe2: Subscription;
   subscription: Subscription;
@@ -28,84 +27,46 @@ export class DisplayComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.httpReq.searchInput = params['searchInput'];
+      this.httpReq.getReqCounter = 0;
+      this.httpReq.determineSections();
+
+      let str = params['id'];
+      if (str) {
+        let n = str.length;
+        let lastChar = str[n - 1];
+        if (lastChar !== '0') {
+          this.subscription = this.httpReq
+            .onSearchWithParams(this.httpReq.searchInput + str)
+            .subscribe((response) => {
+              this.arrayPages = response;
+            });
+          this.local.currentPath =
+            '/admin-search/' + params['searchInput'] + '/' + params['id'];
+        } else {
+          let newstr = <string>str;
+          newstr = newstr.slice(0, n - 1);
+          newstr = newstr + '3';
+          this.subscription = this.httpReq
+            .onSearchWithParams(this.httpReq.searchInput + newstr)
+            .subscribe((response) => {
+              this.arrayPages = response;
+            });
+          this.local.currentPath =
+            '/admin-search/' + params['searchInput'] + '/' + newstr;
+          this.router.navigate([
+            '/admin-search/' + this.httpReq.searchInput + '/' + newstr,
+          ]);
+        }
+      }
+    });
     this.subscribe = this.pagesService.newSection.subscribe(
       (pagesOfThisSection) => {
         this.local.pages = pagesOfThisSection;
         this.arrayPages = this.local.pages;
       }
     );
-    this.route.params.subscribe((params: Params) => {
-      this.httpReq.searchInput = params['searchInput'];
-      let str = params['id'];
-      if (str) {
-        let n = str.length;
-        let lastChar = str[n - 1];
-        let numberOfSection = str[n - 10];
-        if (lastChar !== '0') {
-          console.log('Ultimo carattere diverso a 0');
-          console.log('Numero di sezione scelta: ' + numberOfSection);
-          console.log(
-            'Numero massimo di sezioni: ' + this.httpReq.getReqCounter
-          );
-          if (numberOfSection <= this.httpReq.getReqCounter) {
-            console.log('Sezione scelta più piccola del massimo consentito');
-            this.subscription = this.httpReq
-              .onSearchWithParams(this.httpReq.searchInput + str)
-              .subscribe((response) => {
-                this.local.pages = response;
-                this.arrayPages = response;
-              });
-            this.local.currentPath =
-              '/admin-search/' + params['searchInput'] + '/' + params['id'];
-            this.router.navigate([
-              '/admin-search/' + this.httpReq.searchInput + '/' + params['id'],
-            ]);
-          } else {
-            console.log('Sezione scelta più grande del massimo consentito');
-            let newstr = '&_page=1&_limit=' + lastChar;
-            this.subscription = this.httpReq
-              .onSearchWithParams(this.httpReq.searchInput + newstr)
-              .subscribe((response) => {
-                this.local.pages = response;
-                this.arrayPages = response;
-              });
-            this.local.currentPath =
-              '/admin-search/' + params['searchInput'] + '/' + newstr;
-            this.router.navigate([
-              '/admin-search/' + this.httpReq.searchInput + '/' + newstr,
-            ]);
-          }
-        } else {
-          if (numberOfSection <= this.httpReq.getReqCounter) {
-            let newstr = '&_page=' + numberOfSection + '&_limit=3';
-            this.subscription = this.httpReq
-              .onSearchWithParams(this.httpReq.searchInput + newstr)
-              .subscribe((response) => {
-                this.local.pages = response;
-                this.arrayPages = response;
-              });
-            this.local.currentPath =
-              '/admin-search/' + params['searchInput'] + '/' + newstr;
-            this.router.navigate([
-              '/admin-search/' + this.httpReq.searchInput + '/' + newstr,
-            ]);
-          } else {
-            let newstr = '&_page=1&_limit=3';
-            this.subscription = this.httpReq
-              .onSearchWithParams(this.httpReq.searchInput + newstr)
-              .subscribe((response) => {
-                this.local.pages = response;
-                this.arrayPages = response;
-              });
-            this.local.currentPath =
-              '/admin-search/' + params['searchInput'] + '/' + newstr;
-            this.router.navigate([
-              '/admin-search/' + this.httpReq.searchInput + '/' + newstr,
-            ]);
-          }
-        }
-      }
-    });
   }
 
   onDelete(idPage: number, i: number) {
