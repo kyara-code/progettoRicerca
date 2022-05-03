e0f229f66e43c9db32763054b2978dd7a5c690c3
kyara-code <chiara.ko.26@gmail.com>
2022-05-03 11:34:06 +0200

    project

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,3 @@ if (environment.production) {
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
-
-const app = require('http').createServer((req, res) => res.send('Ahoy!'));
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}`);
-});
